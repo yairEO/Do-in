807a88210108624f989c2062b5e1f4a31065c687
vsync <vsync.design@gmail.com>
2015-06-15 21:00:26 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+do-in
+========
+
+Do something in X time.
+
+A task runner to do any job in a given duration of time.
+(movement of elements is not for this, since it can be done via CSS)
+
+
+##[Demo page](http://codepen.io/vsync/pen/deoxg)
---
+++ do-in.js
@@ -0,0 +1,36 @@
+//// By Yair Even-Or
+//// 2014
+
+
+////// Do X in Y seconds /////////
+function Doin(step, duration, done){
+    this.duration = duration || 2; 
+    this.step = step;
+    this.done = done;
+    this.stop = false;
+};
+
+Doin.prototype = {
+    run : function(){
+        "use strict";
+        var startTime = new Date(),
+        that = this;
+
+        (function run(){
+            var now, elapsed, t;
+
+            now = new Date(); 
+            elapsed = (now - startTime)/1000;
+            t = (elapsed/that.duration);
+
+            // do a step on each frame
+            that.step(t, elapsed); 
+
+            // stop sequence if duration has passed
+            if( t < 1 || that.stop )
+                requestAnimationFrame(run);  // can also use: setTimeout(run, 60/1000) 
+            else if(that.done)
+                that.done();
+        })();
+    }
+}; 
