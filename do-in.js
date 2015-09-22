b06c85d652548a1ab65c97daac31df319b55893c
vsync <vsync.design@gmail.com>
2015-09-22 16:55:50 +0200

    tabs -> spaces  minor text changes in README

--- do-in.js
+++ do-in.js
@@ -4,10 +4,10 @@
 
 ////// Do X in Y seconds /////////
 function Doin(settings){  //step, duration, done
-    this.duration = settings.duration || 2; 
+    this.duration = settings.duration || 2;
     this.step = settings.step;
     this.done = settings.done;
-	this.fps = typeof settings.fps != "undefined" ? settings.fps : null;
+    this.fps = typeof settings.fps != "undefined" && settings.fps > 0 ? settings.fps : null;
     this.RAF;
 };
 
@@ -15,29 +15,29 @@ Doin.prototype = {
     run : function(){
         "use strict";
         cancelAnimationFrame(this.RAF);
-		
+
         var startTime = new Date(),
-			that = this;
+            that = this;
 
         (function run(){
             var now, elapsed, t;
 
-            now = new Date(); 
+            now = new Date();
             elapsed = (now - startTime)/1000;
             t = (elapsed/that.duration);
 
             // do a step on each frame
-            that.step(t, elapsed); 
+            that.step(t, elapsed);
 
             // stop sequence if duration has passed
             if( t < 1 ){
-				if( that.fps )
-					setTimeout(function(){ requestAnimationFrame(run) }, 1000/that.fps);
-				else
-					that.RAF = requestAnimationFrame(run);  // can also use: setTimeout(run, 60/1000)
-			}
+                if( that.fps )
+                    setTimeout(function(){ requestAnimationFrame(run) }, 1000/that.fps);
+                else
+                    that.RAF = requestAnimationFrame(run);  // can also use: setTimeout(run, 60/1000)
+            }
             else if(that.done)
                 that.done();
         })();
     }
-}; 
+};
