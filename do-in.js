fd1ee438c55c7ae1027b53fd855c97071ed5dd76
Yair Even Or <vsync.design@gmail.com>
2021-06-10 10:34:30 +0200

    minor cleanup

--- do-in.js
+++ do-in.js
@@ -7,13 +7,10 @@ function Doin(settings){  //step, duration, done
     this.step = settings.step;
     this.done = settings.done;
     this.fps = typeof settings.fps != "undefined" && settings.fps > 0 ? settings.fps : null;
-    this.RAF;
-    this.startTime;
-    this.isPlaying;
 };
 
 Doin.prototype = {
-    start : function(){
+    start(){
         window.cancelAnimationFrame( this.RAF );
         // if "startTime" is not "null", meaning the animation is not yet finished, so change the start time to compensate for the offset
         this.startTime = this.startTime ? new Date().getTime() - this.pauseTime : new Date().getTime();
@@ -22,7 +19,7 @@ Doin.prototype = {
         this.isPlaying = true;
     },
 
-    play : function(){
+    play(){
         var that = this, now, elapsed, t;
 
         now = new Date();
@@ -34,10 +31,9 @@ Doin.prototype = {
 
         // stop sequence if duration has passed
         if( t < 1 ){
-            if( this.fps )
-                that.RAF = setTimeout(function(){ window.requestAnimationFrame(that.play.bind(that)) }, 1000/this.fps);
-            else
-                this.RAF = window.requestAnimationFrame(this.play.bind(this));
+            that.RAF = this.fps 
+                ? setTimeout(function(){ window.requestAnimationFrame(that.play.bind(that)) }, 1000/this.fps)
+                : this.RAF = window.requestAnimationFrame(this.play.bind(this));
         }
         else{
             this.done && this.done();
@@ -45,7 +41,7 @@ Doin.prototype = {
         }
     },
 
-    pause : function(){
+    pause(){
         window.cancelAnimationFrame( this.RAF );
         clearTimeout( this.RAF );
         this.isPlaying = false;
